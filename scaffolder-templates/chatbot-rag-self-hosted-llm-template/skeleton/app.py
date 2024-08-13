af263674edc5df2250a41c465934ba61eea2e495
treddy08 <treddy@redhat.com>
2024-08-13 23:25:00 +1000

    update

--- scaffolder-templates/chatbot-rag-self-hosted-llm-template/skeleton/app.py
+++ scaffolder-templates/chatbot-rag-self-hosted-llm-template/skeleton/app.py
@@ -110,7 +110,7 @@ Always answer as helpfully as possible, while being safe. Your answers should no
 If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
 <</SYS>>
 
-Context: 
+Context:
 {context}
 
 Question: {question} [/INST]
