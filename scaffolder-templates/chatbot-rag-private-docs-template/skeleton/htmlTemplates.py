e0e49956e55e4ec3d9553c81369a375764af3f4c
treddy08 <treddy@redhat.com>
2024-09-12 20:47:22 +1000

    update

--- scaffolder-templates/chatbot-rag-private-docs-template/skeleton/htmlTemplates.py
+++ scaffolder-templates/chatbot-rag-private-docs-template/skeleton/htmlTemplates.py
@@ -30,7 +30,7 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://cdn-icons-png.flaticon.com/512/6134/6134346.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="https://gpte-public.s3.amazonaws.com/CI+Assets/icons_and_logos/chat-user.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
@@ -40,7 +40,7 @@ user_template = '''
 <div class="chat-message user">
     <div class="message" style="text-align:right">{{MSG}}</div>
     <div class="avatar">
-        <img src="https://png.pngtree.com/png-vector/20190321/ourmid/pngtree-vector-users-icon-png-image_856952.jpg">
+        <img src="https://gpte-public.s3.amazonaws.com/CI+Assets/icons_and_logos/chat-bot.png">
     </div>
 
 </div>
