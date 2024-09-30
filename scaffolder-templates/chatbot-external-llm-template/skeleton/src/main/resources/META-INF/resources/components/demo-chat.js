d9ac9df77ebb81b6cbfdbbcb76add9ec572c8de6
treddy08 <treddy@redhat.com>
2024-09-30 19:19:16 +1000

    update

--- scaffolder-templates/chatbot-external-llm-template/skeleton/src/main/resources/META-INF/resources/components/demo-chat.js
+++ scaffolder-templates/chatbot-external-llm-template/skeleton/src/main/resources/META-INF/resources/components/demo-chat.js
@@ -11,36 +11,60 @@ import '@vaadin/grid';
 import '@vaadin/grid/vaadin-grid-sort-column.js';
 
 export class DemoChat extends LitElement {
-    static styles = css`
-      .button {
-        cursor: pointer;
-      }
-    `;
+
+    _stripHtml(html)   {
+        const div = document.createElement("div");
+        div.innerHTML = html;
+        return div.textContent || div.innerText || "";
+    }
 
     connectedCallback() {
-        var protocol = (window.location.protocol == "https:") ? "wss" : "ws";
+
         const chatBot = document.getElementsByTagName("chat-bot")[0];
 
+        var protocol = (window.location.protocol == "https:") ? "wss" : "ws";
         const socket = new WebSocket(protocol + "://" + window.location.host + "/chatbot");
         socket.onmessage = function (event) {
-            chatBot.sendMessage(event.data, {
-                right: false,
-                sender: {name: 'Bob', id: '007'}
-            });
+            chatBot.hideLastLoading();
+            // LLM response
+            var lastMessage;
+            if (chatBot.messages.length > 0) {
+                lastMessage = chatBot.messages[chatBot.messages.length - 1];
+            }
+            if (lastMessage && lastMessage.sender.id === "llm") {
+                if (! lastMessage.msg) {
+                    lastMessage.msg = "";
+                }
+                lastMessage.msg += event.data;
+                let bubbles = chatBot.shadowRoot.querySelectorAll("chat-bubble");
+                let bubble = bubbles.item(bubbles.length - 1);
+                bubble.innerHTML = lastMessage.msg;
+                chatBot.body.scrollTo({ top: chatBot.body.scrollHeight, behavior: 'smooth' })
+            } else {
+                chatBot.sendMessage(event.data, {
+                    right: false,
+                    continued: true,
+                    sender: {
+                        id: "llm"
+                    }
+                });
+            }
         }
 
+        const that = this;
         chatBot.addEventListener("sent", function (e) {
             if (e.detail.message.right === true) {
                 // User message
-                socket.send(e.detail.message.message);
+                socket.send(that._stripHtml(e.detail.message.message));
                 chatBot.sendMessage("", {
                     right: false,
-                    sender: {name: 'Bob', id: '007'},
                     loading: true
                 });
             }
         });
     }
+
+
 }
 
 customElements.define('demo-chat', DemoChat);
